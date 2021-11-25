c4848862337f038fe5c77efa45a9267fab23f9e9
HALECKIY <privetshkolote99@gmail.com>
2021-11-25 18:20:09 +0700

    Add cat

---
+++ src/Categories.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import {StyleSheet, Text, View} from "react-native";
+
+export const Categories = () => {
+    return (
+        <View style={styles.container}>
+            <View style={styles.header}>
+                <Text>fsdfs</Text>
+            </View>
+            <View>
+                <Text>sdsa</Text>
+            </View>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        alignItems: 'flex-start',
+    },
+    header: {
+        marginBottom: 10,
+        height: 74,
+        width: '100%',
+        backgroundColor: '#4285F4',
+    },
+});
