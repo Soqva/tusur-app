2d86ffd039f815adb497e44a354e37e9043a3999
Nikita <s0qva69@gmail.com>
2021-12-14 21:32:52 +0700

    The DrawerNavigation's content has been customized.
    New content style was added.

---
+++ src/drawer-navigation/CustomDrawerContent.jsx
@@ -0,0 +1,119 @@
+import {Image, StyleSheet, View} from "react-native";
+import {DrawerItem} from "@react-navigation/drawer";
+import React from "react";
+
+export const CustomDrawerContent = ({ navigation }) => {
+    return (
+        <View>
+            <DrawerItem
+                style={styles.item}
+                labelStyle={styles.label}
+                label="Профиль"
+                icon={({focused, color, size}) => (
+                    <Image
+                        source={require('../assets/profile-icon.png')}
+                    />
+                )}
+                onPress={() => {
+                    navigation.navigate('Profile');
+                }}
+            />
+            <DrawerItem
+                style={styles.item}
+                labelStyle={styles.label}
+                label="Вопросы"
+                icon={({focused, color, size}) => (
+                    <Image
+                        source={require('../assets/profile-icon.png')}
+                    />
+                )}
+                onPress={() => {
+                    navigation.navigate('Tasks');
+                }}
+            />
+            <DrawerItem
+                style={styles.item}
+                labelStyle={styles.label}
+                label="Избранное"
+                icon={({focused, color, size}) => (
+                    <Image
+                        source={require('../assets/favorites-icon.png')}
+                    />
+                )}
+                onPress={() => {
+                    navigation.navigate('Favorites');
+                }}
+            />
+            <DrawerItem
+                style={styles.item}
+                labelStyle={styles.label}
+                label="Уведомления"
+                icon={({focused, color, size}) => (
+                    <Image
+                        source={require('../assets/notifications-icon.png')}
+                    />
+                )}
+                onPress={() => {
+                    navigation.navigate('Notifications');
+                }}
+            />
+            <DrawerItem
+                style={styles.item}
+                labelStyle={styles.label}
+                label="Поделиться"
+                icon={({focused, color, size}) => (
+                    <Image
+                        source={require('../assets/share-icon.png')}
+                    />
+                )}
+                onPress={() => {
+                    navigation.navigate('Share');
+                }}
+            />
+            <DrawerItem
+                style={styles.item}
+                labelStyle={styles.label}
+                label="Настройки"
+                icon={({focused, color, size}) => (
+                    <Image
+                        source={require('../assets/settings-icon.png')}
+                    />
+                )}
+                onPress={() => {
+                    navigation.navigate('Settings');
+                }}
+            />
+            <DrawerItem
+                style={styles.downItem}
+                labelStyle={styles.downLabel}
+                label="Отключить рекламу"
+            />
+            <DrawerItem
+                labelStyle={styles.downLabel}
+                label="Связаться с нами"
+            />
+            <DrawerItem
+                labelStyle={styles.downLabel}
+                label="О приложении"
+            />
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    item: {
+        paddingTop: 30,
+    },
+    label: {
+        color: "white",
+        fontSize: 18,
+        paddingLeft: 20,
+    },
+    downItem: {
+        marginTop: 50,
+    },
+    downLabel: {
+        color: "white",
+        fontSize: 12,
+    },
+});
